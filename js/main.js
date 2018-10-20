026faae38ff6f4ea82c71adbe007cf6bf938adb4
ilyas zemrani <ilyas.gkeeper@gmail.com>
2018-10-20 08:49:16 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+/** Pagination function */
+
+function ShowPage(id)
+{
+    var numberOfPages = 4;
+    
+    for(i = 0; i <= numberOfPages; i++)
+    {
+        if(document.getElementById('page' + i))
+            document.getElementById('page' + i).style.display = 'none';
+    }
+
+    if(document.getElementById('page' + id))
+        document.getElementById('page' + id).style.display = 'block';
+}
+
+
